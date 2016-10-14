e638595f65fbd8875cabadf6e7696ed87d14e5e1
Raphael Fonseca <raphaelfonseca88@hotmail.com>
2016-10-14 14:54:33 -0700

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+ // This program display the number of days in each month
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int MONTHS = 12;
+	int days[MONTHS] = { 31, 28, 31, 30,
+						31, 30, 31, 31,
+						30, 31, 30, 31 };
+
+	for (int count = 0; count < MONTHS; count++)
+	{
+		cout << "Month " << (count + 1) << " has ";
+		cout << days[count] << " days.\n";
+	}
+    return 0;
+}
+
